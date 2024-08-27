50ab33cfa84a439452d1dbb9c3a2e615a979de58
NarrowVertex <walterchoi3916@gmail.com>
2024-08-27 20:30:58 +0900

    Separate _get_page() method from switch_page() in PageManager.py

--- system/PageManager.py
+++ system/PageManager.py
@@ -15,9 +15,12 @@ class PageManager:
         self.page_map[type(page).__name__] = page
 
     def switch_page(self, page_name):
+        st.session_state["page"] = self._get_page(page_name)
+        st.rerun()
+
+    def _get_page(self, page_name):
         if page_name not in self.page_map:
             print(f"No page named '{page_name}' is found!")
-            return
+            return None
 
-        st.session_state["page"] = self.page_map[page_name]
-        st.rerun()
+        return self.page_map[page_name]
